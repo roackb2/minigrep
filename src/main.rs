3707e3a14ec42eabf0d256591b511cb6cf8ce9cd
Jay/Fienna Liang <roackb2@gmail.com>
2021-12-27 23:07:57 +0800

    first commit

---
+++ src/main.rs
@@ -0,0 +1,11 @@
+use std::env;
+use std::fs;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    let query = &args[1];
+    let filename = &args[2];
+
+    println!("searching '{}' in file '{}'", query, filename);
+}
